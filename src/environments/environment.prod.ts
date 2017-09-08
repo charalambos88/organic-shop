440dc0af9272eff106ec53efbab8da31e3c037b0
charalambos88 <charalambos88@users.noreply.github.com>
2017-09-08 09:24:20 +0300

    Angular CLI ready

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+	production: true,
+	firebase: {
+		apiKey: "AIzaSyAvLwX-o0QIRr4oFqqkChumgg13VVp5NxI",
+		authDomain: "oshop-webone.firebaseapp.com",
+		databaseURL: "https://oshop-webone.firebaseio.com",
+		projectId: "oshop-webone",
+		storageBucket: "",
+		messagingSenderId: "161138780942"
+	}
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+  	    apiKey: "AIzaSyAvLwX-o0QIRr4oFqqkChumgg13VVp5NxI",
+    authDomain: "oshop-webone.firebaseapp.com",
+    databaseURL: "https://oshop-webone.firebaseio.com",
+    projectId: "oshop-webone",
+    storageBucket: "",
+    messagingSenderId: "161138780942"
+  }
 };
